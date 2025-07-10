c81e81934a632dbc0a13840ec96dd504d5f31bb1
anisoncloud <anisoncloud@gmail.com>
2025-07-10 12:38:38 +0600

    Roles add

---
+++ MyErp/Controllers/RoleController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyErp.Models;
+using MyErp.ViewModels;
+
+namespace MyErp.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly UserManager<Users> _userManager;
+        private readonly SignInManager<Users> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(UserManager<Users> userManager, SignInManager<Users> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles.ToList();
+            return View(roles);
+        }
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateRole(string roleName)
+        {
+            if (string.IsNullOrEmpty(roleName))
+            {
+                ModelState.AddModelError("", "Role name cannot be empty.");
+                return View();
+            }
+            if (!await _roleManager.RoleExistsAsync(roleName))
+            {
+                var role = new IdentityRole(roleName);
+                var result = await _roleManager.CreateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Role already exists.");
+            }
+            return View();
+        }
+    }
+}
---
+++ MyErp/ViewModels/RoleViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MyErp.ViewModels
+{
+    public class RoleViewModel
+    {
+        public string RoleName { get; set; }
+    }
+}
