f5b7cd75cd06e9f4ad8e3da997cc118136a99a10
anisoncloud <anisoncloud@gmail.com>
2025-08-04 18:28:47 +0600

    Post View Model Created

---
+++ MyErp/Controllers/PostController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyErp.Data;
+
+namespace MyErp.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly AppDbContex _context;
+
+        public PostController(AppDbContex context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var posts = _context.Posts.ToList();
+            return View(posts);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyErp/ViewModels/PostViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MyErp.ViewModels
+{
+    public class PostViewModel
+    {
+        public string PostName { get; set; }
+        public string Body { get; set; }
+        public List<int> SelectPostCategoryIds { get; set; }
+        public List<SelectListItem> AllPostCategories { get; set; }
+    }
+}
