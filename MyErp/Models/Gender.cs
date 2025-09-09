bcc238e8c800e2123d248c80e96dc986fe033408
anisoncloud <anisoncloud@gmail.com>
2025-09-09 18:34:16 +0600

    Ok

---
+++ MyErp/Models/Gender.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyErp.Models
+{
+    public enum Gender
+    {
+        [EnumMember(Value ="MALE")]
+        Male,
+        [EnumMember(Value ="FEMALE")]
+        Female
+    }
+}
