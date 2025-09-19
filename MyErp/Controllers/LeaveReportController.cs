8147f4029766233a35d3e96fc2f28db17c8013ab
anisoncloud <anisoncloud@gmail.com>
2025-09-19 12:51:14 +0600

    Sick

---
+++ MyErp/Controllers/LeaveReportController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyErp.Data;
+using MyErp.Models;
+using MyErp.ViewModels;
+
+namespace MyErp.Controllers
+{
+    public class LeaveReportController : Controller
+    {
+        private readonly AppDbContex _context;
+        private readonly SignInManager<Users> _signInManager;
+        private readonly UserManager<Users> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public LeaveReportController(SignInManager<Users> signInManager, UserManager<Users> userManager, RoleManager<IdentityRole> roleManager, AppDbContex context)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var leaveAllocation = _context.LeaveAllocations.FirstOrDefault(x => x.EmpId == user.Id);
+            var enjoyedLeave = _context.LeaveRequests.Where(x => x.EmpId == user.Id && x.LeaveStauts == "Approved" && x.RequestDate.Year==DateTime.Now.Year).ToList();
+            var vm = new LeaveReportViewModel
+            {
+                Users = user,
+                LeaveAllocation = leaveAllocation,
+                LeaveRequest = enjoyedLeave
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ MyErp/ViewModels/LeaveReportViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MyErp.Models;
+
+namespace MyErp.ViewModels
+{
+    public class LeaveReportViewModel
+    {
+        public Users Users { get; set; }
+        public LeaveAllocation LeaveAllocation { get; set; }
+        public List<LeaveRequest> LeaveRequest { get; set; }
+
+        public int CalculateTotalSick()
+        {
+            int totalSick = 0;
+            foreach (var item in LeaveRequest)
+            {
+                totalSick += item.Days; 
+            }
+            return totalSick;
+        }
+    }
+}
