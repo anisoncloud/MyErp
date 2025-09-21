d600e5cfbcc936b1dbf8b93e742102a4511592d5
anisoncloud <anisoncloud@gmail.com>
2025-09-21 21:57:33 +0600

    CrmCompany

---
+++ MyErp/Migrations/20250921134922_UpdateCrmCompany.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyErp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCrmCompany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactPerson",
+                table: "CrmCompanies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactPersonEmail",
+                table: "CrmCompanies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactPersonPhone",
+                table: "CrmCompanies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "CrmCompanies",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "CrmCompanies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Designation",
+                table: "CrmCompanies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "CrmCompanies",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "CrmCompanies",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactPerson",
+                table: "CrmCompanies");
+
+            migrationBuilder.DropColumn(
+                name: "ContactPersonEmail",
+                table: "CrmCompanies");
+
+            migrationBuilder.DropColumn(
+                name: "ContactPersonPhone",
+                table: "CrmCompanies");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "CrmCompanies");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "CrmCompanies");
+
+            migrationBuilder.DropColumn(
+                name: "Designation",
+                table: "CrmCompanies");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "CrmCompanies");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "CrmCompanies");
+        }
+    }
+}
--- MyErp/Models/CrmCompany.cs
+++ MyErp/Models/CrmCompany.cs
@@ -1,13 +1,33 @@
-﻿namespace MyErp.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyErp.Models
 {
-    public class CrmCompany
+    public class CrmCompany : BaseEntity
     {
         public int ID  { get; set; }
+        [DisplayName("Full Name of Company")]
         public string Name { get; set; }
-        public string? Description { get; set; }
+        [DisplayName("Details about company")]
+        public string? Description { get; set; }        
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        [DisplayName("Company Email")]
         public string? CompanyEmail { get; set; }
+        [DisplayName("Company's Phone Number")]
         public string? CompanyPhone { get; set; }
+        [DisplayName("Company's Address/Location")]
         public string? CompanyAddress {  get; set; }
+        [DisplayName("Contact Person Full Name")]
+        public string? ContactPerson {  get; set; }
+        [DisplayName("Designation")]
+        public string? Designation {  get; set; }
+        [DisplayName("Contact Person's Mobile")]
+        public string? ContactPersonPhone {  get; set; }
+        [DisplayName("Contact Person's Email")]
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public string? ContactPersonEmail {  get; set; }
         public ICollection<Domain>? Domains { get; set; }
         public ICollection<CrmContact>? CrmContacts { get; set; }
     }
