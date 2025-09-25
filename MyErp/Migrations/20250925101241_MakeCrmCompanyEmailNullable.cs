6caeb3c4a68413f72ac9e39db0b9633bac708418
anisoncloud <anisoncloud@gmail.com>
2025-09-25 16:23:13 +0600

    updated CrmCompany Nullable

---
+++ MyErp/Migrations/20250925101241_MakeCrmCompanyEmailNullable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyErp.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeCrmCompanyEmailNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            DECLARE @sql NVARCHAR(MAX) = N'';
+            SELECT @sql += 'ALTER TABLE CrmCompany DROP CONSTRAINT ' + QUOTENAME(dc.name) + ';'
+            FROM sys.default_constraints dc
+            INNER JOIN sys.columns c ON c.default_object_id = dc.object_id
+            WHERE dc.parent_object_id = OBJECT_ID('CrmCompany') AND c.name = 'CompanyEmail';
+            EXEC sp_executesql @sql;
+            ");
+
+            migrationBuilder.AlterColumn<string>(
+            name: "CompanyEmail",
+            table: "CrmCompanies",
+            type: "nvarchar(max)",
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+            name: "CompanyEmail",
+            table: "CrmCompanies",
+            type: "nvarchar(max)",
+            nullable: false,
+            defaultValue: "",
+            oldClrType: typeof(string),
+            oldType: "nvarchar(max)",
+            oldNullable: true);
+        }
+    }
+}
