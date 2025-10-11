73cc1256c459bc957ac885b8b055afe87bcba23b
anisoncloud <anisoncloud@gmail.com>
2025-10-12 00:54:13 +0600

    Some update

---
+++ MyErp/Controllers/CreditVoucherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyErp.Controllers
+{
+    public class CreditVoucherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
