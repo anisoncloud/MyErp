4561965757bec5d3d5c978381390c9c6fefd0ec8
Anisur Rahman <anisoncloud@gmail.com>
2025-12-13 23:01:39 +0600

    some updates

---
+++ MyErp/Models/EmployeeSalary.cs
@@ -0,0 +1,15 @@
+﻿namespace MyErp.Models
+{
+    public class EmployeeSalary
+    {
+        public int Id { get; set; }
+        public string? UserId {  get; set; }
+        public int? BasicSalary {  get; set; }
+        public int? HouseRent {  get; set; }
+        public int? MedicalAllowance { get; set; }
+        public int? DearnessAllowance {  get; set; }
+        public int? MobileAllowance {  get; set; }
+        public int? FoodSubsidy { get; set; }
+        public Users? Users { get; set; }
+    }
+}
